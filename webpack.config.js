efeb9253938dad58a0d0b39e1427cd1ac7ed0ea4
kellyfire <kellyfire611@gmail.com>
2024-07-04 07:49:52 +0700

    Bổ sung cấu hình webpack + các components

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,32 @@
-const path = require('path')
+const path = require("path");
+var fs = require("fs");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// Look for .html files
+var htmlFiles = [];
+var directories = ["src"];
+while (directories.length > 0) {
+  var directory = directories.pop();
+  var dirContents = fs
+    .readdirSync(directory)
+    .map((file) => path.join(directory, file));
+
+  htmlFiles.push(...dirContents.filter((file) => file.endsWith(".html")));
+  directories.push(
+    ...dirContents.filter((file) => fs.statSync(file).isDirectory())
+  );
+}
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 3000,
     open: true,
@@ -20,10 +37,32 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        test: /\.(scss|css)$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name]-[hash][ext]",
+        },
       },
     ],
   },
-}
+  plugins: [
+    // Build a new plugin instance for each .html file found
+    ...htmlFiles.map(
+      (htmlFile) =>
+        new HtmlWebpackPlugin({
+          template: htmlFile,
+          filename: htmlFile.replace(path.normalize("src/"), ""),
+          inject: false,
+        })
+    ),
+  ],
+};
